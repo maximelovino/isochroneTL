20f0d34cb3bba6ae516d014c94fd0e5fa46ae0df
Maxime Lovino <maxime.lovino@epfl.ch>
2014-02-25 08:55:14 +0100

    added javaDoc for Stop

--- ch.epfl.isochrone/src/ch/epfl/isochrone/timetable/Stop.java
+++ ch.epfl.isochrone/src/ch/epfl/isochrone/timetable/Stop.java
@@ -2,23 +2,46 @@ package ch.epfl.isochrone.timetable;
 
 import ch.epfl.isochrone.geo.PointWGS84;
 
+/**
+ * A transit stop
+ * 
+ * @author Maxime Lovino (236726)
+ * @author Julie Djeffal (193164)
+ * 
+ *
+ */
 public final class Stop {
     private final String name;
     private final PointWGS84 position;
     
+    /**
+     * @param name
+     *      The name of the stop
+     * @param position
+     *      The position in the WGS84 coordinates
+     */
     public Stop(String name, PointWGS84 position){
         this.name=name;
         this.position=position;
     }
     
+    /**
+     * @return The name of the stop
+     */
     public String name(){
         return name;
     }
     
+    /**
+     * @return The position of the stop as WGS84 point
+     */
     public PointWGS84 position(){
         return position;
     }
     
+    /* (non-Javadoc)
+     * @see java.lang.Object#toString()
+     */
     public String toString(){
         return name;
     }
