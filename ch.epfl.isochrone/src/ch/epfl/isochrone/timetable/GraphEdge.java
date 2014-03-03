491d32a644418e59a8b77efc31711fb32a3fc8f0
Maxime Lovino <maximelovino@gmail.com>
2014-03-03 21:44:51 +0100

    TO DO: earliestArrivalTime()

--- ch.epfl.isochrone/src/ch/epfl/isochrone/timetable/GraphEdge.java
+++ ch.epfl.isochrone/src/ch/epfl/isochrone/timetable/GraphEdge.java
@@ -1,52 +1,75 @@
 package ch.epfl.isochrone.timetable;
 
+import java.util.HashSet;
+import java.util.Set;
+import static ch.epfl.isochrone.math.Math.*;
+
 final class GraphEdge {
-    
+    private final Set<Integer> packedTrips;
+    private final Stop destination;
+    private final int walkingTime;
     
     public GraphEdge(Stop destination, int walkingTime, Set<Integer> packedTrips) throws IllegalArgumentException{
-        
+        this.destination=destination;
+        this.walkingTime=walkingTime;
+        this.packedTrips=new HashSet(packedTrips);
     }
     
     public static int packTrip(int departureTime, int arrivalTime) throws IllegalArgumentException{
+        if(departureTime<0||departureTime>107999){
+            throw new IllegalArgumentException();
+        }
+        if(arrivalTime-departureTime<0||arrivalTime-departureTime>9999){
+            throw new IllegalArgumentException();
+        }
         
+        int packedTrip=departureTime*10000+(arrivalTime-departureTime);
+        
+        return packedTrip;
     }
     
     public static int unpackDepartureTime(int packedTrip){
-        
+        return divF(packedTrip,10000);
     }
     
     public static int unpackTripDuration(int packedTrip){
-        
+        return modF(packedTrip,10000);
     }
     
     public static int unpackArrivalTime(int packedTrip){
-        
+        return unpackDepartureTime(packedTrip)+unpackTripDuration(packedTrip);
     }
     
-    private Stop destination(){
-        
+    public Stop destination(){
+        return destination;
     }
     
-    private int earliestArrivalTime(int departureTime){
+    public int earliestArrivalTime(int departureTime){
         
     }
     
     public static class Builder{
+        private final Stop destination;
+        private int walkingTime;
+        private final Set<Integer> packedTrips;
         
         public Builder(Stop destination){
-            
+            this.destination=destination;
+            this.packedTrips=new HashSet<Integer>();
         }
         
         public Builder setWalkingTime(int newWalkingTime){
+            this.walkingTime=newWalkingTime;
             return this;
         }
         
         public Builder addTrip(int departureTime, int arrivalTime){
+            packedTrips.add(packTrip(departureTime, arrivalTime));
             return this;
         }
         
         public GraphEdge build(){
-            return new GraphEdge(destination, walkingTime, packedTrips);
+            return new GraphEdge(this.destination, this.walkingTime, this.packedTrips);
         }
         
     }
