352d7c0a8ccffcd6fbdf68c1599c6a28f98d9ff7
Maxime Lovino <maximelovino@gmail.com>
2014-03-12 14:07:17 +0100

    TO DO: WEEK 5

--- ch.epfl.isochrone/src/ch/epfl/isochrone/timetable/Graph.java
+++ ch.epfl.isochrone/src/ch/epfl/isochrone/timetable/Graph.java
@@ -1,7 +1,9 @@
 package ch.epfl.isochrone.timetable;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 public final class Graph {
@@ -9,16 +11,25 @@ public final class Graph {
     private Graph(Set<Stop> stops, Map<Stop, List<GraphEdge>> outgoingEdges){
         
     }
-    
+//    totally false certainly, need to understand how it works
     public final class Builder{
         private final Set<Stop> stops;
         private final List<GraphEdge> tripEdges;
+        private Map<Stop, Map<Stop, GraphEdge.Builder>> buildingEdges;
+        private Map<Stop, GraphEdge.Builder> nestBuildingEdges;
         
         public Builder(Set<Stop> stops){
             this.stops=new HashSet<Stop>(stops);
+            this.tripEdges=new ArrayList<GraphEdge>();
+            for(Stop aStop:stops){
+                nestBuildingEdges.put(aStop, new GraphEdge.Builder(aStop));
+                buildingEdges.put(aStop, nestBuildingEdges);
+            }
+            
         }
         
         public Builder addTripEdge(Stop fromStop, Stop toStop, int departureTime, int arrivalTime){
+            
             return this;
         }
         
