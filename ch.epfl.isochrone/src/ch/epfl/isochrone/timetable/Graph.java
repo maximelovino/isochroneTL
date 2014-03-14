4d17cedb8d28cadcd024b9eb07113169e64700f0
Maxime Lovino <maximelovino@gmail.com>
2014-03-14 12:29:54 +0100

    Graph

--- ch.epfl.isochrone/src/ch/epfl/isochrone/timetable/Graph.java
+++ ch.epfl.isochrone/src/ch/epfl/isochrone/timetable/Graph.java
@@ -1,6 +1,7 @@
 package ch.epfl.isochrone.timetable;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
@@ -11,30 +12,42 @@ public final class Graph {
     private Graph(Set<Stop> stops, Map<Stop, List<GraphEdge>> outgoingEdges){
         
     }
-//    totally false certainly, need to understand how it works
+    
     public final class Builder{
         private final Set<Stop> stops;
         private final List<GraphEdge> tripEdges;
         private Map<Stop, Map<Stop, GraphEdge.Builder>> buildingEdges;
-        private Map<Stop, GraphEdge.Builder> nestBuildingEdges;
         
         public Builder(Set<Stop> stops){
             this.stops=new HashSet<Stop>(stops);
             this.tripEdges=new ArrayList<GraphEdge>();
-            for(Stop aStop:stops){
-                nestBuildingEdges.put(aStop, new GraphEdge.Builder(aStop));
-                buildingEdges.put(aStop, nestBuildingEdges);
+            this.buildingEdges=new HashMap<Stop, Map<Stop, GraphEdge.Builder>>();            
+        }
+        
+        public Builder addTripEdge(Stop fromStop, Stop toStop, int departureTime, int arrivalTime) throws IllegalArgumentException{
+            if(!this.stops.contains(fromStop)||!this.stops.contains(toStop)||departureTime<0||arrivalTime<0||arrivalTime<departureTime){
+                throw new IllegalArgumentException();
             }
             
-        }
-        
-        public Builder addTripEdge(Stop fromStop, Stop toStop, int departureTime, int arrivalTime){
             
             return this;
         }
         
-        public Builder addAllWalkEdges(int maxWalkingTime, double walkingSpeed){
+        public Builder addAllWalkEdges(int maxWalkingTime, double walkingSpeed) throws IllegalArgumentException{
+            if(maxWalkingTime<0||walkingSpeed<=0){
+                throw new IllegalArgumentException();
+            }
             return this;
+        }
+        
+        private Map<Stop, GraphEdge.Builder> createMap(Stop toStop, int departureTime, int arrivalTime){
+            GraphEdge.Builder b=new GraphEdge.Builder(toStop);
+            b.addTrip(departureTime, arrivalTime);
+            
+            Map<Stop, GraphEdge.Builder> m=new HashMap<Stop, GraphEdge.Builder>();
+            m.put(toStop, b);
+            
+            return m;
         }
         
         public Graph build(){
